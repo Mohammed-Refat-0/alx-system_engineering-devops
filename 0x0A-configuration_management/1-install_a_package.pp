999149ebc11235722a1009e106e818dd5438a7d6
Mohammed Refat <mohammedrefat53@outlook.com>
2024-03-23 00:15:44 +0200

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 # Install a version of flask (2.1.0)
 
-package {'flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
-package {'werkzeug':
+package { 'werkzeug':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
