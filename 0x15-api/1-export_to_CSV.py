d3b82468d5cc9dca447d932cd4b230ffcedbfdf3
Mohammed Refat <mohammedrefat53@outlook.com>
2024-04-23 23:10:27 +0200

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that use a REST API for a website with a given employee ID,
+to returns information about thier TODO list progress,
+and export it in csv format."""
+
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(id)).json()
+    todo = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    file_name = id + '.csv'
+    with open(file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+
+        for i in todo:
+            writer.writerow(
+                [id, user.get("name"), i.get("completed"), i.get("title")])
