bb84003af64d15842022541183501bf2b45d63f8
Mohammed Refat <mohammedrefat53@outlook.com>
2024-06-04 12:55:57 +0300

    task 0 -fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,15 +2,16 @@
 """ the number of subscribers for a given subreddit.
 If an invalid subreddit is given, return 0.
 """
-import requests
 
 
 def number_of_subscribers(subreddit):
     """function that queries the Reddit API
     and returns the number of subscribers"""
+    import requests
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     response = requests.get(url)
-    if response.status_code == 404 or response.status_code >= 300:
+    if response.status_code >= 300:
         return 0
     else:
         return response.json().get("data", {}).get("subscribers", 0)
