2810a38c822eebb6df2d899d5d835bd3f19cd6cf
Mohammed Refat <mohammedrefat53@outlook.com>
2024-06-04 12:53:24 +0300

    task 0 -fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     and returns the number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     response = requests.get(url)
-    if response.status_code == 404 or response.status_code == 301:
+    if response.status_code == 404 or response.status_code >= 300:
         return 0
     else:
         return response.json().get("data", {}).get("subscribers", 0)
